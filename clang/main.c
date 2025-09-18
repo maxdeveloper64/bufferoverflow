d9f1c749d4eeb2ceed3f9ed9919d9dce90068348
Max <max.developer64@gmail.com>
2025-09-18 15:39:56 +0000

    fourth commit

---
+++ clang/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+int matrix[2][3] = {{1,2,3}, {4,5,6}};
+int line[3] = {1,2,3};
+
+int main(int argc, char *argv[]) {
+	int i, j;
+	
+	printf("main started \n");
+
+	for(i = 0; i < 2; i++) {
+		for(j = 0; j < 3; j++) {
+			printf("%d:", matrix[i][j]);
+		}
+		printf("\n");
+	}
+	printf("%d:\n", *(line + 1));
+	printf("=====\n");
+	int *ptr = &matrix[0][0];
+	for(i = 0; i < 2; i++) {
+		for(j = 0; j < 3; j++) {
+			//printf("i:%d, j:%d\n", i,j);
+			//printf("%d;", **(matrix + ((i*3) + j)));
+			printf("%d|", *(ptr + (i*3 +j)));
+			//printf("%d-", **(matrix + (1+2)));
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
