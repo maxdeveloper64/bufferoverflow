5a9e339dea1144f82b529ba82df28dc24a63e312
Max <max.developer64@gmail.com>
2025-09-22 13:20:05 +0000

    new dir

--- clang/main.c
+++ clang/main.c
@@ -22,11 +22,21 @@ int main(int argc, char *argv[]) {
 		for(j = 0; j < 3; j++) {
 			//printf("i:%d, j:%d\n", i,j);
 			//printf("%d;", **(matrix + ((i*3) + j)));
-			printf("%d|", *(ptr + (i*3 +j)));
+			printf("%d|", *((ptr + (i*3)) +j));
 			//printf("%d-", **(matrix + (1+2)));
 		}
 		printf("\n");
 	}
+
+	for(i = 0; i < 2; i++) {
+		for(j = 0; j < 3; j++) {
+			printf("i: %d, j: %d", i,j);
+			int *pr = *(matrix + (i * 3));
+			printf("%d-",*(*(matrix + (i * 3)) + j));
+		}
+		printf("\n");
+	}
+
 	return 0;
 }
 
